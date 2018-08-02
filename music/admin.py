e361724b6fe447fa3ee288496db136c931dbd348
zacjact1568 <511470661@qq.com>
2018-08-02 09:48:15 +0800

    在管理员页面显示添加时间

--- music/admin.py
+++ music/admin.py
@@ -3,7 +3,7 @@ from .models import Music
 
 
 class MusicAdmin(admin.ModelAdmin):
-    list_display = ['title', 'artist', 'album']
+    list_display = ['title', 'artist', 'album', 'time']
 
 
 admin.site.register(Music, MusicAdmin)
